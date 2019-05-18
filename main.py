17be67b4905f579257e050a14bca536335ba8c71
Kreando <49857899+kreando@users.noreply.github.com>
2019-05-18 14:00:10 +0200

    modificaciones segun lecciones

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 class Circuito():
     corredores=[]
     __posStartY=(-30, -10, 10, 30)
@@ -9,7 +10,7 @@ class Circuito():
         self.__screen.setup(width, height)
         self.__screen.bgcolor("lightgray")
         self.__startLine= -width/2+(width*0.05)
-        self.__finishLine= width/2-(width*0.05)
+        self.__finishLine= width/2-(width*0.04)
         
         self.__createRunners()
         
@@ -23,6 +24,23 @@ class Circuito():
             
             self.corredores.append(new_turtle)
 
+    def competir(self):
+        hayGanador=False
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance = random.randint(1,6)
+                tortuga.forward(avance)
+                
+                if tortuga.pos()[0] >= self.__finishLine:
+                    hayGanador=True
+                    return print("\nla tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                    #en lugar de return que compi sugirió, ramon puso break debajo
+                    #break
 if __name__=="__main__":
     circuito=Circuito(640, 480)
     print(circuito.corredores[2].pos())
+    print(circuito.corredores[2].pos()[0])
+    print(circuito.corredores[2].pos()[1])
+    print(circuito.corredores[2].color())
+    circuito.competir()
+    
